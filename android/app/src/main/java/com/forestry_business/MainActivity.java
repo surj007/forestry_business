507d53e4ea1a074311556fcb78b584856b134697
surongjian <surongjian@kingsoft.com>
2019-02-20 14:27:27 +0800

    init

---
+++ android/app/src/main/java/com/forestry_business/MainActivity.java
@@ -0,0 +1,15 @@
+package com.forestry_business;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "forestry_business";
+    }
+}
